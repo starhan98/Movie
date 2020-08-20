20b3ffcca89c7a6b9b0bfe18fa0984b80775e205
Sang Yup Han <gtd9872@gmail.com>
2020-08-21 01:39:28 +0900

    4주차 #4 done.

--- app/src/main/java/com/example/movie/MovieAdapter.kt
+++ app/src/main/java/com/example/movie/MovieAdapter.kt
@@ -9,13 +9,24 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 
+// GSON은 JSON 데이터의 변수의 이름과 data class의 변수의 이름을 각각 비교해서, 이름이 똑같은 값이 있으면 알맞게 넣어준다.
+
+// 영화 정보를 담고있는 data class
+// JSON의 변수명과 Movie의 변수명이 일치해야 한다.
 data class Movie(
     val title : String,
     val popularity : Double,
-    val description : String,
-    val openDate : String,
-    val posterUrl : Int
+    val overview : String,
+    val release_date : String,
+    val poster_path : String
+)
+
+// ArrayList에 대한 data class
+// JSON과 일치시켜줘야하므로 변수명은 results가 된다.
+data class MovieList(
+    val results: ArrayList<Movie>
 )
 
 class MovieAdapter(val context: Context, val movieList: ArrayList<Movie>) : RecyclerView.Adapter<MovieAdapter.Holder>() {
@@ -48,11 +59,19 @@ class MovieAdapter(val context: Context, val movieList: ArrayList<Movie>) : Recy
 
         // 데이터를 셀에 넣는 역할.
         fun bind(movie: Movie){
-            imgPoster.setImageResource(movie.posterUrl)
+            val overview: String
+            if(movie.overview.length > 21) {
+                overview = movie.overview.slice(IntRange(0, 20)) + "..."
+            }
+            else{
+                overview = movie.overview
+            }
+
+            Glide.with(context).load("https://image.tmdb.org/t/p/w500" + movie.poster_path).into(imgPoster)
             tvTitle.text = movie.title
             tvPopularity.text = "인기도 : " + movie.popularity // 문자열 + 실수형으로 쓰면 문자열으로 자동 변환
-            tvDescription.text = "설명 : " + movie.description
-            tvOpenDate.text = "개봉일 : " + movie.openDate
+            tvDescription.text = "설명 : " + overview
+            tvOpenDate.text = "개봉일 : " + movie.release_date
 
             container.setOnClickListener { // 셀을 클릭했을 때 영화 제목을 토스트 메지로 띄움
                 Toast.makeText(context, movie.title, Toast.LENGTH_SHORT).show()
--- app/src/main/java/com/example/movie/MovieListActivity.kt
+++ app/src/main/java/com/example/movie/MovieListActivity.kt
@@ -8,18 +8,12 @@ import com.android.volley.Request
 import com.android.volley.RequestQueue
 import com.android.volley.Response
 import com.android.volley.toolbox.Volley
+import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_movie_list.*
 import org.json.JSONException
 import com.android.volley.toolbox.JsonObjectRequest as JsonObjectRequest
 
 class MovieListActivity : AppCompatActivity() {
-
-    val movieList : ArrayList<Movie> = arrayListOf(
-        Movie("테넷", 22.433, "시간의 흐름은...", "2020-08-26", R.drawable.m2),
-        Movie("소년 시절의 너", 17.321, "넌 세상을 지켜,...", "2020-07-09", R.drawable.m4),
-        Movie("덩케르크 이스케이프", 15.842, "역사에 기록되 않은...", "2020-08-03", R.drawable.m3),
-        Movie("짱구는 못말려: 신혼여행 허리케인", 10.954, "짱구 THE Movie...", "2020-08-07", R.drawable.m1)
-    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,18 +22,25 @@ class MovieListActivity : AppCompatActivity() {
         // Volley의 RequestQueue 선언
         var requestQueue: RequestQueue = Volley.newRequestQueue(this)
 
+        // GSON 객체 선언
+        var gson: Gson = Gson()
+
         // API 주소 선언.
         val url = "https://api.themoviedb.org/3/movie/now_playing?" + "api_key=f3c242ff274a50ec9b2ccb6c033fe7ec" + "&language=ko-KR" + "&region=KR"
 
         // API를 호출함.
         val request = JsonObjectRequest(Request.Method.GET, url, null,
         Response.Listener { // 데이터가 정상적으로 호출됐을 때 처리하는 부분.
-            response -> try {
-                // response(영화 JSON 데이터)가 정상적으로 넘어온 경우.
-                // 받아온 json 데이터를 Toast message로 출력.
-                Toast.makeText(this, response.toString(), Toast.LENGTH_SHORT).show()
-            } catch (e: JSONException) { 
-                // response가 정상적으로 넘어오지 않은 경우.
+            response -> try { // response(영화 JSON 데이터)가 정상적으로 넘어온 경우.
+                // response(영화 JSON 데이터) -> MovieList data class로 변형. (by GSON)
+                val data = gson.fromJson<MovieList>(response.toString(), MovieList::class.java)
+
+                val adapter = MovieAdapter(this, data.results) // Adapter 선언
+                movieRecycler.adapter = adapter // RecyclerView에 우리가 만든 MovieAdapter 셋팅
+
+                val lm = LinearLayoutManager(this) // LinearLayoutManager 선언 - RecyclerView의 방향을 결정함 (기본이 위에서 아래, 수평도 가능)
+                movieRecycler.layoutManager = lm // RecyclerView에 LinearLayoutManager 선언
+            } catch (e: JSONException) { // response가 정상적으로 넘어오지 않은 경우.
                 // 오류 내용을 Toast로 출력
                 Toast.makeText(this, e.localizedMessage, Toast.LENGTH_SHORT).show()
             }
@@ -50,12 +51,5 @@ class MovieListActivity : AppCompatActivity() {
 
         requestQueue.add(request)
 
-
-        // 이전 코드
-        val adapter = MovieAdapter(this, movieList) // Adapter 선언
-        movieRecycler.adapter = adapter // RecyclerView에 우리가 만든 MovieAdapter 셋팅
-
-        val lm = LinearLayoutManager(this) // LinearLayoutManager 선언 - RecyclerView의 방향을 결정함 (기본이 위에서 아래, 수평도 가능)
-        movieRecycler.layoutManager = lm // RecyclerView에 LinearLayoutManager 선
     }
 }
