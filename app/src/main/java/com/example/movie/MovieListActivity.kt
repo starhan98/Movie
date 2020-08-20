0ce0987094090c94f98b20c9e3ede846a38582f3
Sang Yup Han <gtd9872@gmail.com>
2020-08-20 23:03:39 +0900

    4주차 Json 받아오다 오류남...

--- app/src/main/java/com/example/movie/MovieListActivity.kt
+++ app/src/main/java/com/example/movie/MovieListActivity.kt
@@ -2,8 +2,15 @@ package com.example.movie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.activity_movie_list.*
+import org.json.JSONException
+import com.android.volley.toolbox.JsonObjectRequest as JsonObjectRequest
 
 class MovieListActivity : AppCompatActivity() {
 
@@ -18,7 +25,34 @@ class MovieListActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_list)
 
-       val adapter = MovieAdapter(this, movieList) // Adapter 선언
+        // Volley의 RequestQueue 선언
+        var requestQueue: RequestQueue = Volley.newRequestQueue(this)
+
+        // API 주소 선언.
+        val url = "https://api.themoviedb.org/3/movie/now_playing?" + "api_key=f3c242ff274a50ec9b2ccb6c033fe7ec" + "&language=ko-KR" + "&region=KR"
+
+        // API를 호출함.
+        val request = JsonObjectRequest(Request.Method.GET, url, null,
+        Response.Listener { // 데이터가 정상적으로 호출됐을 때 처리하는 부분.
+            response -> try {
+                // response(영화 JSON 데이터)가 정상적으로 넘어온 경우.
+                // 받아온 json 데이터를 Toast message로 출력.
+                Toast.makeText(this, response.toString(), Toast.LENGTH_SHORT).show()
+            } catch (e: JSONException) { 
+                // response가 정상적으로 넘어오지 않은 경우.
+                // 오류 내용을 Toast로 출력
+                Toast.makeText(this, e.localizedMessage, Toast.LENGTH_SHORT).show()
+            }
+        }, Response.ErrorListener { // 데이터가 정상적으로 호출되지 않았을 때 처리하는 부분.
+            // 오류 내용을 Toast로 출력
+            error -> Toast.makeText(this, error.localizedMessage, Toast.LENGTH_SHORT).show()
+        })
+
+        requestQueue.add(request)
+
+
+        // 이전 코드
+        val adapter = MovieAdapter(this, movieList) // Adapter 선언
         movieRecycler.adapter = adapter // RecyclerView에 우리가 만든 MovieAdapter 셋팅
 
         val lm = LinearLayoutManager(this) // LinearLayoutManager 선언 - RecyclerView의 방향을 결정함 (기본이 위에서 아래, 수평도 가능)
